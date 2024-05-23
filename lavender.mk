275a5b2f8ba442ce6fdaad3d47f7ec0d28c67ec0
Envoy-Z-Lab <envoyzlab@gmail.com>
2024-05-23 23:40:41 +0530

    lavender: Bringup for @LeafOS-Project

--- lavender.mk
+++ lavender.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2018-2019 The LeafOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,13 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_NAME := lineage_lavender
+# Inherit some common LeafOS stuff
+$(call inherit-product, vendor/leaf/config/common_full_phone.mk)
+
+WITH_GMS := true
+
+PRODUCT_NAME := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
