6c7e5d7bc169e2a8b05ab2db15f7ab22770847e7
Envoy-Z-Lab <envoyzlab@gmail.com>
2024-05-23 23:52:39 +0530

    lavender: Genesis-ify

--- device.mk
+++ device.mk
@@ -299,7 +299,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-genesis
 
 PRODUCT_PACKAGES += \
     SettingsOverlay \
