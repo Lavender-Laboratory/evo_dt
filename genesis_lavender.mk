09e12773e4ac98e1e1a22946609d3982999ad289
Envoy-Z-Lab <envoyzlab@gmail.com>
2024-05-23 23:52:39 +0530

    lavender: Bringup for @GenesisOS

--- genesis_lavender.mk
+++ genesis_lavender.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common GenesisOS stuff
+$(call inherit-product, vendor/genesis/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_NAME := lineage_lavender
+PRODUCT_NAME := genesis_lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
